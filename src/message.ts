53722b434066715bbf8c6f381fcc125af8b9fe69
Kyle Fang <zhigang1992@gmail.com>
2025-03-20 01:55:56 +0000

    feat: add message field support as well

--- src/message.ts
+++ src/message.ts
@@ -2,6 +2,7 @@ export type RichMessage = {
   channel?: string;
   event: string;
   text?: string;
+  message?: string;
   emoji?: string;
   metadata?: Record<string, string>;
   notify?: boolean;
@@ -11,17 +12,17 @@ export function formatRichMessage(message: RichMessage): string {
   const metadata = Object.entries(message.metadata ?? {})
     .map(([key, value]) => `#${key}: ${value}`)
     .join("\n");
-  return `${message.emoji ? message.emoji + " • " : ""}${
+  return `${message.emoji ? `${message.emoji} • ` : ""}${
     message.channel
       ? `<ins>#${message.channel}</ins>
 
 `
       : ""
   }<b>${message.event}</b>${
-    message.text
+    (message.text ?? message.message)
       ? `
 
-<code>${message.text}</code>`
+<code>${message.text ?? message.message}</code>`
       : ""
   }
 
